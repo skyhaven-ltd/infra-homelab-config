c392d12564f2ba2e96cf1f84facaf31bd9071865
Liam Goodchild <liamgoodchild12@hotmail.co.uk>
2026-07-26 16:01:33 +0100

    Enable the k3s subnet route declaratively

--- terraform/tailscale/_outputs.tf
+++ terraform/tailscale/_outputs.tf
@@ -7,3 +7,8 @@ output "key_expiry_disabled" {
   description = "Whether Tailscale key expiry is disabled for the k3s node"
   value       = tailscale_device_key.k8s.key_expiry_disabled
 }
+
+output "subnet_routes" {
+  description = "Subnet routes enabled for the k3s node"
+  value       = tailscale_device_subnet_routes.k8s.routes
+}
